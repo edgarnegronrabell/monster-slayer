154c9209dcbcbd73ab28415bf13e8c21f3ac457c
Edgar Negrón <edgar.negron.rabell@gmail.com>
2019-12-22 09:14:48 -0400

    Add and refactor Action buttons, start working on Damage Inflicted list

--- app.js
+++ app.js
@@ -3,37 +3,86 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameStarted: false,
+    gameIsRunning: false,
     healtbarResponseStyle: {
       width: '100%'
     },
-
+    turns: []
   },
   methods: {
     startGame() {
-      this.gameStarted = !this.gameStarted;
-    },
-    giveUpGame() {
-      this.startGame();
+      this.gameIsRunning = !this.gameIsRunning;
       this.playerHealth = 100;
       this.monsterHealth = 100;
     },
-    playerAttack() {
-      let random = Math.floor((Math.random() * 10));
-      console.log(random);
-      if(this.monsterHealth > 0) {
-       return this.monsterHealth -= random
+    
+    attack() {
+      this.playerAttack();
+      this.turns.unshift({
+        isPlayer: true,
+        
+      });
+      if(this.checkWin()) {
+        return;
       }
+      this.monsterAttack();
+    },
+    specialAttack() {
+      this.monsterHealth -= this.calculateDamage(10, 20);
+      this.turns.unshift(this.monsterAttack);
+      if(this.checkWin()) {
+        return;
+      }
+      this.monsterAttack();
+      this.turns
+    },
+    heal() {
+      if(this.playerHealth <= 90) {
+        this.playerHealth += this.calculateDamage(10, 15);
+      } else {
+        this.playerHealth = 100;
+      }
+      this.monsterAttack();
+    },
+    giveUp() {
+      this.startGame();
     },
     monsterAttack() {
-      let random = Math.floor((Math.random() * 13));
-      console.log(random);
-      if(this.playerHealth > 0) {
-        return this.playerHealth -= random 
-    }
+      this.playerHealth -= this.calculateDamage(5, 13); 
+      this.checkWin();
+    },
+    playerAttack() {
+      this.monsterHealth -= this.calculateDamage(3, 10);
+    },
+    calculateDamage(min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min)
+    },
+    checkWin() {
+      if(this.monsterHealth <= 0) {
+        if(confirm('You won! Play again?')) {
+          this.startGame();
+        } else {
+          gameIsRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if(confirm('You lost! Play again?')) {
+          this.startGame();
+        } else {
+          gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
+    },
+    pushTurns(attacker) {
+      return this.turns.unshift(attacker)
+    } 
+  },
+  computed: {
+    playerDamageInflicted() {
+      return `You have been dealt ${this.attack}`
+    },
+    monsterDamageInflicted() {}
   }
-}
-  // computed: {
-
-  // }
 });
